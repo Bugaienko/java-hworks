1e4bfda7e4ea6d0ac6147049eb46d734b4e01dee
Bugaienko <sergiibugaienko@gmail.com>
2022-08-09 10:52:17 +0200

    hw01 add

---
+++ Hw_01/src/Main.java
@@ -0,0 +1,21 @@
+//Задача 1: 3, 4
+//Задача 2: 2, 5, 6
+//Задача 3: 3, 4, 5
+//Задача 4: 2, 3
+//Задача 5: 3, 4
+//Задача 6: Две пустые строки и третья строка "**". В ответах нет такого варианта
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("     **");
+        System.out.println("     **");
+        System.out.println("     **");
+        System.out.println("     **");
+        System.out.println("**   **");
+        System.out.println("  ***  ");
+
+
+    }
+}
